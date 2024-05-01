a53a1dfe6e28932501dc0e6766883154ab306059
manyworldss <Ralphsuber@gmail.com>
2024-05-01 00:48:30 -0400

    Initial Commit

---
+++ Simple Weather App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Weather App
+//
+//  Created by Rapheal Suber on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Weather App/Simple_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Weather_AppApp.swift
+//  Simple Weather App
+//
+//  Created by Rapheal Suber on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
